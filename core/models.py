90ebd4261ea9c8b926f36c79cc4d1f0a1fd8c8b1
oegpyg <oegpyg@gmail.com>
2014-08-16 22:11:01 -0400

    Commit desde pycharm3

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here
 class Company(models.Model):
-    pass
+    Code = models.CharField(max_length=15, unique=True, null=False)
 
 
 class Office(models.Model):
