5cab222e11175f388563ebf9cdb34323f2bd2e1f
oegpyg <oegpyg@gmail.com>
2014-08-24 20:06:26 -0400

    Modules Search in object

--- GlobalStatic/js/sbwindows.js
+++ GlobalStatic/js/sbwindows.js
@@ -8,20 +8,138 @@ $.expr[':'].icontains = function(obj, index, meta, stack){
     return (obj.textContent || obj.innerText || jQuery(obj).text() || '').toLowerCase().indexOf(meta[3].toLowerCase()) >= 0;
 };
 
-modules = false;
-$(document).ready(function(){
-    //pedro definime como es la url para traer los modulos disponibles
-    var l = window.location;
-    $.ajax({
-        type: "GET",
-        dataType: 'json',
-        url: l.protocol + '//' + l.host + '/WSmodules/',
-        success: function(data){
-            modules = data;
+var DataSearch = {
+    'modules': {},
+    getModules:function(url){
+        $.ajax({
+            type: "GET",
+            dataType: 'json',
+            url: url,
+            success: function(data){
+                this.modules = data;
+            }
+        });
+    },
+    search: function(tables, ele){
+        var table = $(tables);
+        $.each(this.modules, function( key, module ) {
+            $.each(module, function(index, val){
+                table.append('<tr class="hide run"><td style="">'+key+'</td><td class="" data-module="'+val[0]+'">'+val[1]+'</td></tr>')
+            });
+        });
+        $(ele).on('focus',function(){
+            $('#sb-search-modules').on('keyup',function(event){
+                event.preventDefault();
+                if ($(this).val().length > 3){
+                    table.find('tr').addClass('hide');
+                    var buscar = $(this).val();
+                    if(jQuery.trim(buscar) != ''){
+                        $(tables+" tr:icontains('" + buscar + "')").removeClass('hide');
+                    }
+                }
+                if ($(this).val().length < 3){
+                    table.find('tr').addClass('hide');
+                }
+            });
+        });
+        $('.run').bind('click', function(){
+            this.ListWindow($(this).find('td').eq(1).text(),
+                $(this).find('td').eq(0).text(),
+                $(this).find('td').eq(1).attr('data-module')
+            );
+        });
+    },
+    //ListWindow: function(title, module, record){
+    ListWindow: function(title, url, module_name, method_name, class_name){
+        //sujeto a cambios solo es una maqueta
+        var html = '';
+        $.ajax({
+            type: "GET",
+            dataType: 'html',
+            //url: l.protocol + '//' + l.host + '/'+module+'/load/'+record+'/',
+            url: url + module_name + '/' + method_name + '/' + class_name,
+            success: function(data){
+                DataSearch.Appwindow(title, data);
+                $('#'+class_name+'List').dataTable({
+                    "oLanguage": {
+                        "sProcessing":     "Procesando...",
+                        "sLengthMenu":     "Mostrar _MENU_ registros",
+                        "sZeroRecords":    "No se encontraron resultados",
+                        "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                        "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                        "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+                        "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                        "sInfoPostFix":    "",
+                        "sSearch":         "Buscar:",
+                        "sUrl":            "",
+                        "sInfoThousands":  ",",
+                        "sLoadingRecords": "Cargando...",
+                        "oPaginate": {
+                            "sFirst":    "Primero ",
+                            "sLast":     " Último",
+                            "sNext":     " Siguiente",
+                            "sPrevious": "Anterior "
+                        },
+                        "oAria": {
+                            "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                        }
+                    }
+                });
+            }
+        });
+    },
+    Appwindow: function(title, content) {
+        /*Contruye la ventana a partir de los parametros asignados por ListWindow()
+         * Manejando una ventana con funciones independientes
+         * */
+        var div_count = $('.dialog_window').length + 1;
+        //generate a unique id based on the total number
+        var div_id = 'dialog_window_' + div_count;
+        var buttons = new Array();
+        if( $('#alertbutton').is(':checked') ) {
+            buttons.push({
+                text: 'ALERT',
+                click: function() {
+                    alert('ALERTING from Dialog Widnow: ' + title);
+                }
+            });
         }
-    });
-    //por ahora cargo a mano modules
-    modules = {"Ventas": [
+        if( $('#closebutton').is(':checked') ) {
+            buttons.push({
+                text: 'CLOSE',
+                click: function() {
+                    $('#' + div_id).dialog('close');
+                }
+            });
+        }
+        //append the dialog window HTML to the body
+        $('body').append('<div class="dialog_window" id="' + div_id + '">' + content + '</div>');
+        //initialize our new dialog
+        var dialog = $('#' + div_id).dialog({
+            width: 'auto',
+            height: 'auto',
+            title : title,
+            autoOpen : true,
+            buttons: buttons
+        });
+        $('#dialog_window_'+div_count).find(':input').each(
+                    function(){
+                        var newId = this.id + div_count;
+                        $(this).prev().attr('for', newId); // update label for (assume prev sib is label)
+                        this.name = this.id = newId; // update id and name (assume the same)
+                    });
+        $('#dialog_window_'+div_count+' #recent-tab li a').each(function(){
+            $(this).attr('href', $(this).attr('href')+div_count);
+        });
+        $('#dialog_window_'+div_count+' div.tab-pane').each(function(){
+            var newId = this.id + div_count;
+            this.name = this.id = newId;
+        });
+    }
+};
+$(document).ready(function(){
+    DataSearch.modules = {"Ventas": [
                            ["ItemGroup", "Grupos de Articulo"],
                            ["ItemTaxGroup", "Origen de Articulo"],
                            ["Item", "Articulos"]
@@ -47,135 +165,10 @@ $(document).ready(function(){
                            ["User", "Usuarios"]
                           ]
     };
-    table = $('table#DataSearch');
-        $.each(modules, function( key, module ) {
-            $.each(module, function(index, val){
-                table.append('<tr class="hide run"><td style="">'+key+'</td><td class="" data-module="'+val[0]+'">'+val[1]+'</td></tr>')
-        });
-    });
-    $('#sb-search-modules').on('focus',function(){
-        list = $('table#DataSearch');
-        $('#sb-search-modules').on('keyup',function(event){
-            event.preventDefault();
-            if ($(this).val().length > 3){
-                list.find('tr').addClass('hide');
-                buscar = $(this).val();
-                if(jQuery.trim(buscar) != ''){
-                    $("table#DataSearch tr:icontains('" + buscar + "')").removeClass('hide');
-                }
-            }
-            if ($(this).val().length < 3){
-                list.find('tr').addClass('hide');
-            }
-        });
-    });
+    DataSearch.search('table#DataSearch','#sb-search-modules');
 
 });
 
-function ListWindow(title, module, record){
-    var l = window.location;
-    var html = '';
-    $.ajax({
-        type: "GET",
-        dataType: 'html',
-        url: l.protocol + '//' + l.host + '/'+module+'/load/'+record+'/',
-        success: function(data){
-            Appwindow(title, data);
-            $('#'+record+'List').dataTable({
-                "oLanguage": {
-                    "sProcessing":     "Procesando...",
-                    "sLengthMenu":     "Mostrar _MENU_ registros",
-                    "sZeroRecords":    "No se encontraron resultados",
-                    "sEmptyTable":     "Ningún dato disponible en esta tabla",
-                    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-                    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-                    "sInfoPostFix":    "",
-                    "sSearch":         "Buscar:",
-                    "sUrl":            "",
-                    "sInfoThousands":  ",",
-                    "sLoadingRecords": "Cargando...",
-                    "oPaginate": {
-                        "sFirst":    "Primero ",
-                        "sLast":     " Último",
-                        "sNext":     " Siguiente",
-                        "sPrevious": "Anterior "
-                    },
-                    "oAria": {
-                        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-                    }
-                }
-            });
-        }
-    });
-}
-
-
-function run(title, module, record){
-    /*Constructor del Modulo
-    * Aqui se debe realizar todas la opciones para la ventana
-    * */
-    var l = window.location;
-    var html = '';
-    $.ajax({
-        type: "GET",
-        dataType: 'html',
-        url: l.protocol + '//' + l.host + '/'+module+'/load/'+record+'/',
-        success: function(data){
-            Appwindow(title, data);
-        }
-    });
-}
-
-function Appwindow(title, content) {
-    /*Contruye la ventana a partir de los parametros asignados por run()
-    * Manejando una ventana con funciones independientes
-    * */
-    var div_count = $('.dialog_window').length + 1;
-    //generate a unique id based on the total number
-    var div_id = 'dialog_window_' + div_count;
-    var buttons = new Array();
-    if( $('#alertbutton').is(':checked') ) {
-        buttons.push({
-            text: 'ALERT',
-            click: function() {
-                alert('ALERTING from Dialog Widnow: ' + title);
-            }
-        });
-    }
-    if( $('#closebutton').is(':checked') ) {
-        buttons.push({
-            text: 'CLOSE',
-            click: function() {
-                $('#' + div_id).dialog('close');
-            }
-        });
-    }
-    //append the dialog window HTML to the body
-    $('body').append('<div class="dialog_window" id="' + div_id + '">' + content + '</div>');
-    //initialize our new dialog
-    var dialog = $('#' + div_id).dialog({
-        width: 'auto',
-        height: 'auto',
-        title : title,
-        autoOpen : true,
-        buttons: buttons
-    });
-    $('#dialog_window_'+div_count).find(':input').each(
-                function(){
-                    var newId = this.id + div_count;
-                    $(this).prev().attr('for', newId); // update label for (assume prev sib is label)
-                    this.name = this.id = newId; // update id and name (assume the same)
-                });
-    $('#dialog_window_'+div_count+' #recent-tab li a').each(function(){
-        $(this).attr('href', $(this).attr('href')+div_count);
-    });
-    $('#dialog_window_'+div_count+' div.tab-pane').each(function(){
-        var newId = this.id + div_count;
-        this.name = this.id = newId;
-    });
-}
 //oef added with og 2014-08-24
 
 
