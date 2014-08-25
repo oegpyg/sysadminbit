4322fc4216735c4006966a70e11110f628919503
oegpyg <oegpyg@gmail.com>
2014-08-24 20:30:04 -0400

    postgresspass

--- GlobalStatic/js/sbwindows.js
+++ GlobalStatic/js/sbwindows.js
@@ -8,6 +8,7 @@ $.expr[':'].icontains = function(obj, index, meta, stack){
     return (obj.textContent || obj.innerText || jQuery(obj).text() || '').toLowerCase().indexOf(meta[3].toLowerCase()) >= 0;
 };
 
+
 var DataSearch = {
     'modules': {},
     getModules:function(url){
