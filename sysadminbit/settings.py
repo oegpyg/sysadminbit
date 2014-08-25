b744fa78b789f0de63fd3d5396b1ad6e99be0525
oegpyg <oegpyg@gmail.com>
2014-08-24 20:15:25 -0400

    postgresspass

--- sysadminbit/settings.py
+++ sysadminbit/settings.py
@@ -69,7 +69,7 @@ DATABASES = {
         'NAME': 'sysadminbit',
         'HOST': 'localhost',
         'USER': 'sysadminbit',
-        'PASSWORD': '75ukuY0m1',
+        'PASSWORD': 'LorenzoLamas',
         'PORT': ''
     }
 }
